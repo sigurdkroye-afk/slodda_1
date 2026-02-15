edc38bc692e2877560e5b7149a5f56cf5df02411
sigurdkroye-afk <sigurd.kr.oye@gmail.com>
2026-02-15 16:51:04 +0100

    feat: add slodda_bringup package with hello_robot test node

---
+++ slodda_bringup/setup.py
@@ -0,0 +1,34 @@
+from setuptools import os
+from glob import glob
+from setuptools import find_packages, setup
+
+package_name = 'slodda_bringup'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+	(os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.py'))),
+        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.yaml'))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='sigurdkroye-afk',
+    maintainer_email='sigurd.kr.oye@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'hello_robot = slodda_bringup.hello_robot:main',
+        ],
+    },
+)
---
+++ slodda_bringup/slodda_bringup/hello_robot.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""
+Hello Robot Node - Test node for Slødda 1
+Publiserer test cmd_vel kommandoer for å verifisere oppsett.
+"""
+
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+
+class HelloRobot(Node):
+    def __init__(self):
+        super().__init__('hello_robot')
+        
+        # Publisher for cmd_vel
+        self.publisher = self.create_publisher(Twist, '/cmd_vel', 10)
+        
+        # Timer - publish hvert sekund
+        self.timer = self.create_timer(1.0, self.publish_command)
+        
+        # Counter
+        self.count = 0
+        
+        self.get_logger().info('🤖 Hello Robot node started!')
+        self.get_logger().info('   Publishing to /cmd_vel')
+    
+    def publish_command(self):
+        msg = Twist()
+        
+        # Enkel oscillerende bevegelse
+        if self.count % 4 < 2:
+            msg.linear.x = 0.2
+        else:
+            msg.linear.x = -0.2
+        
+        self.publisher.publish(msg)
+        self.get_logger().info(f'Publishing cmd_vel [count={self.count}]: linear.x={msg.linear.x:.2f}')
+        
+        self.count += 1
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = HelloRobot()
+    
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        node.get_logger().info('Shutting down Hello Robot node...')
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
