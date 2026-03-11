00c6a653b1d318be7faf8a36d0fe86e8f9ac522b
sigurdkroye-afk <sigurd.kr.oye@gmail.com>
2026-03-11 17:42:18 +0100

    stuqa rundt i gazebo, fått lagt inn urdf chassis men den ligger sidelengs og hjul/belter er fucka

---
+++ slodda_description/launch/view_real_robot.launch.py
@@ -0,0 +1,45 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import xacro
+
+def generate_launch_description():
+    pkg_path = get_package_share_directory('slodda_description')
+    xacro_file = os.path.join(pkg_path, 'urdf', 'slodda_real.urdf.xacro')
+    robot_description = xacro.process_file(xacro_file).toxml()
+    
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='screen',
+        parameters=[{
+            'robot_description': robot_description,
+            'use_sim_time': False
+        }]
+    )
+    
+    joint_state_publisher_gui = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='joint_state_publisher_gui',
+        output='screen'
+    )
+    
+    rviz_config = os.path.join(pkg_path, 'config', 'view_real_robot.rviz')
+    
+    rviz = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', rviz_config]
+   
+    )
+    
+    return LaunchDescription([
+        robot_state_publisher,
+        joint_state_publisher_gui,
+        rviz
+    ])
