afaddab75f7ec0e9d52f71794f2e1b535e18eeb9
sigurdkroye-afk <sigurd.kr.oye@gmail.com>
2026-03-13 10:45:47 +0100

    feat: add arena world and working Gazebo launch with mesh path fix

--- slodda_gazebo/launch/gazebo.launch.py
+++ slodda_gazebo/launch/gazebo.launch.py
@@ -1,67 +1,69 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess
+from launch.actions import ExecuteProcess, SetEnvironmentVariable
 from launch_ros.actions import Node
 import xacro
 
 def generate_launch_description():
-    
     pkg_gazebo = get_package_share_directory('slodda_gazebo')
     pkg_description = get_package_share_directory('slodda_description')
-    
-    world_file = os.path.join(pkg_gazebo, 'worlds', 'empty.sdf')
-    xacro_file = os.path.join(pkg_description, 'urdf', 'slodda_simple.urdf.xacro')
+
+    # Install share-rooten – Gazebo trenger denne for å finne meshes
+    install_share = os.path.join(pkg_description, '..', '..', '..', '..')
+
+    world_file = os.path.join(pkg_gazebo, 'worlds', 'arena.sdf')
+    xacro_file = os.path.join(pkg_description, 'urdf', 'slodda_real.urdf.xacro')
     robot_description = xacro.process_file(xacro_file).toxml()
-    
-    gazebo_server = ExecuteProcess(
-        cmd=['gz', 'sim', '-r', '-s', world_file],
-        output='screen'
-    )
-    
-    gazebo_client = ExecuteProcess(
-        cmd=['gz', 'sim', '-g'],
-        output='screen'
-    )
-    
-    robot_state_publisher = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        name='robot_state_publisher',
-        output='screen',
-        parameters=[{
-            'robot_description': robot_description,
-            'use_sim_time': True
-        }]
-    )
-    
-    spawn_robot = Node(
-        package='ros_gz_sim',
-        executable='create',
-        arguments=[
-            '-name', 'slodda',
-            '-topic', '/robot_description',
-            '-x', '0.0',
-            '-y', '0.0',
-            '-z', '0.1'
-        ],
-        output='screen'
-    )
-    
-    bridge = Node(
-        package='ros_gz_bridge',
-        executable='parameter_bridge',
-        arguments=[
-            '/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist',
-            '/odom@nav_msgs/msg/Odometry@gz.msgs.Odometry'
-        ],
-        output='screen'
-    )
-    
+
     return LaunchDescription([
-        gazebo_server,
-        gazebo_client,
-        robot_state_publisher,
-        spawn_robot,
-        bridge
+
+        # Fortell Gazebo hvor ROS-pakker ligger
+        SetEnvironmentVariable(
+            'GZ_SIM_RESOURCE_PATH',
+            os.path.join(pkg_description, '..')
+        ),
+
+        ExecuteProcess(
+            cmd=['gz', 'sim', world_file],
+            output='screen'
+        ),
+
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{
+                'robot_description': robot_description,
+                'use_sim_time': True
+            }]
+        ),
+
+        Node(
+            package='ros_gz_sim',
+            executable='create',
+            name='spawn_robot',
+            output='screen',
+            arguments=[
+                '-name', 'slodda',
+                '-x', '0', '-y', '0', '-z', '0.1',
+                '-topic', 'robot_description'
+            ]
+        ),
+
+        Node(
+            package='ros_gz_bridge',
+            executable='parameter_bridge',
+            name='bridge',
+            output='screen',
+            arguments=[
+                '/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist',
+                '/odom@nav_msgs/msg/Odometry@gz.msgs.Odometry',
+                '/tf@tf2_msgs/msg/TFMessage@gz.msgs.Pose_V',
+                '/ir_front_left@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan',
+                '/ir_front_center@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan',
+                '/ir_front_right@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan',
+            ]
+        ),
     ])
