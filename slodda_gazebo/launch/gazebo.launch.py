89a29723ae4d03dca4d23da2bc0abf21042e6760
sigurdkroye-afk <sigurd.kr.oye@gmail.com>
2026-03-16 18:18:19 +0100

    fix: IR sensors working with headless-rendering ogre2

--- slodda_gazebo/launch/gazebo.launch.py
+++ slodda_gazebo/launch/gazebo.launch.py
@@ -25,7 +25,7 @@ def generate_launch_description():
         ),
 
         ExecuteProcess(
-            cmd=['gz', 'sim', world_file, '--render-engine-server', 'ogre2', '--render-engine-gui', 'ogre2'],
+            cmd=['gz', 'sim', world_file, '--headless-rendering', '--render-engine-server', 'ogre2'],
             output='screen'
         ),
 
