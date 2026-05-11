305daede4780f3f1e1c9323de0d363c113f06052
sigurdkroye-afk <sigurd.kr.oye@gmail.com>
2026-05-11 23:23:35 +0200

    fix: install.sh riktig repo-rot (../../..)

--- src/slodda_bringup/scripts/install.sh
+++ src/slodda_bringup/scripts/install.sh
@@ -2,7 +2,7 @@
 # Kopierer Python-noder og launch/config-filer fra src/ til install/
 # Kjøres etter git pull på Pi (unngår colcon rebuild).
 set -e
-REPO="$(cd "$(dirname "$0")/../.." && pwd)"
+REPO="$(cd "$(dirname "$0")/../../.." && pwd)"
 SHARE="$REPO/install/slodda_bringup/share/slodda_bringup"
 PYSITE="$REPO/install/slodda_bringup/lib/python3.12/site-packages/slodda_bringup"
 
